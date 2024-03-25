e15ed085218ec598fde418278db3f49d0f0db59e
Juicy_Jesse <91599008+JesseElbertsen@users.noreply.github.com>
2024-03-25 15:41:19 +0100

    filter 2.0

--- buurbak/src/app/Components/Filter/RangeSlider.tsx
+++ buurbak/src/app/Components/Filter/RangeSlider.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 function RangeSlider() {
-  const [minPrice, setMinPrice] = useState(1000);
-  const [maxPrice, setMaxPrice] = useState(7000);
-  const min = 100;
-  const max = 10000;
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(200);
+  const min = 0;
+  const max = 200;
   const [minThumb, setMinThumb] = useState(
     ((minPrice - min) / (max - min)) * 100
   );
@@ -13,13 +13,17 @@ function RangeSlider() {
   );
 
   const minTrigger = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newMinPrice = Math.min(parseInt(event.target.value), maxPrice - 500);
+    let newMinPrice = parseInt(event.target.value);
+    if (newMinPrice < min) newMinPrice = min;
+    if (newMinPrice > maxPrice) newMinPrice = maxPrice;
     setMinPrice(newMinPrice);
     setMinThumb(((newMinPrice - min) / (max - min)) * 100);
   };
 
   const maxTrigger = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newMaxPrice = Math.max(parseInt(event.target.value), minPrice + 500);
+    let newMaxPrice = parseInt(event.target.value);
+    if (newMaxPrice > max) newMaxPrice = max;
+    if (newMaxPrice < minPrice) newMaxPrice = minPrice;
     setMaxPrice(newMaxPrice);
     setMaxThumb(100 - ((newMaxPrice - min) / (max - min)) * 100);
   };
@@ -30,22 +34,22 @@ function RangeSlider() {
         <div>
           <input
             type="range"
-            step="100"
+            step="1"
             min={min}
             max={max}
             onChange={(event) => minTrigger(event)}
             value={minPrice}
-            className="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer"
+            className="absolute z-20 h-2 w-full opacity-0 cursor-pointer"
           />
 
           <input
             type="range"
-            step="100"
+            step="1"
             min={min}
             max={max}
             onChange={(event) => maxTrigger(event)}
             value={maxPrice}
-            className="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer"
+            className="absolute z-20 h-2 w-full opacity-0 cursor-pointer"
           />
 
           <div className="relative z-10 h-2">
@@ -57,12 +61,12 @@ function RangeSlider() {
             ></div>
 
             <div
-              className="absolute z-30 w-6 h-6 top-0 left-0 bg-orange-300 rounded-full -mt-2 -ml-1"
+              className="absolute z-30 w-6 h-6 top-0 left-0 bg-orange-300 rounded-full -mt-2 -ml-1 cursor-pointer"
               style={{ left: `${minThumb}%` }}
             ></div>
 
             <div
-              className="absolute z-30 w-6 h-6 top-0 right-0 bg-orange-300 rounded-full -mt-2 -mr-3"
+              className="absolute z-30 w-6 h-6 top-0 right-0 bg-orange-300 rounded-full -mt-2 -mr-3 cursor-pointer"
               style={{ right: `${maxThumb}%` }}
             ></div>
           </div>
@@ -72,7 +76,8 @@ function RangeSlider() {
           <div>
             <input
               type="number"
-              maxLength={5}
+              min={min}
+              max={maxPrice}
               onChange={(event) => minTrigger(event)}
               value={minPrice}
               className="px-3 py-2 border border-gray-200 rounded w-24 text-center"
@@ -81,7 +86,8 @@ function RangeSlider() {
           <div>
             <input
               type="number"
-              maxLength={5}
+              min={minPrice}
+              max={max}
               onChange={(event) => maxTrigger(event)}
               value={maxPrice}
               className="px-3 py-2 border border-gray-200 rounded w-24 text-center"
